17c263a86fc0f1a5e2851e712eaf5146c7f6c2d0
lamafab <42901763+lamafab@users.noreply.github.com>
2022-02-28 18:34:21 +0000

    implement handleProposed

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,6 +1,6 @@
 import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
+import {Proposed} from "../types";
+import {Balance, PropIndex} from "@polkadot/types/interfaces";
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
     /*
@@ -12,4 +12,16 @@ export async function handleEvent(event: SubstrateEvent): Promise<void> {
     record.field3 = (balance as Balance).toBigInt();
     await record.save();
     */
+}
+
+export async function handleProposed(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [prop_idx, deposit]}} = event;
+    const e = new Proposed(`${event.block.block.header.number}-${event.idx.toString()}`);
+
+    e.block = event.block.block.hash.toHuman();
+    e.timestamp = new Date().toISOString();
+    e.proposal_index = (prop_idx as PropIndex).toNumber();
+    e.deposit = (deposit as Balance).toBigInt();
+
+    await e.save();
 }
