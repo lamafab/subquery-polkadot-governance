dd2804b6622712f1519979b694242870bb27b010
lamafab <42901763+lamafab@users.noreply.github.com>
2022-03-03 21:06:29 +0100

    implement handleExternalTabled

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,5 +1,5 @@
 import { SubstrateExtrinsic, SubstrateEvent, SubstrateBlock } from "@subql/types";
-import { Proposed, Tabled } from "../types";
+import { Proposed, Tabled, ExternalTabled } from "../types";
 import { Balance, PropIndex, AccountId } from "@polkadot/types/interfaces";
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
@@ -36,6 +36,16 @@ export async function handleTabled(event: SubstrateEvent): Promise<void> {
     e.deposit = (deposit as Balance).toBigInt();
     e.depositors = ((depositors as unknown) as Array<AccountId>)
         .map(account => { return account.toString() });
+
+    await e.save();
+}
+
+export async function handleExternalTabled(event: SubstrateEvent): Promise<void> {
+    const { event: { data: [prop_idx, deposit, depositors] } } = event;
+    const e = new Tabled(`${event.block.block.header.number}-${event.idx.toString()}`);
+
+    e.block = event.block.block.hash.toHuman();
+    e.timestamp = new Date().toISOString();
 
     await e.save();
 }
